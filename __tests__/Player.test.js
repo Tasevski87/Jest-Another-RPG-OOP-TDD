029627e260666a99a47d4c2f6e95cb31d02e2dfe
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-03 16:40:33 -0500

    mocks

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,8 +1,8 @@
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion');
 
-jest.mock('../lib/Potion');
-console.log(new Potion());
+jest.mock('../lib/Potion.js');
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -13,5 +13,24 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
 
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-    
+
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or return false', ()=>{
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
